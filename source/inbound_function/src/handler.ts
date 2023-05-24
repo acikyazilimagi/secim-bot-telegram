e3bc7511f22830b8a08d7c59d14c1aaf867ac918
alimli <haldun.alimli@gmail.com>
2023-05-24 08:50:44 +0300

    Fixed problem in queueing outgoing messages

--- source/inbound_function/src/handler.ts
+++ source/inbound_function/src/handler.ts
@@ -5,35 +5,50 @@ import { TelegramMessage } from "./application/telegramMessage";
 import { v4 as uuidv4 } from "uuid";
 
 export const handler = async (event: LambdaFunctionEvent, context: Context) => {
-
   try {
-    const bodyMessage = Buffer.from(event.Records[0].body, "base64").toString(
-      "binary"
-    );
-    handleRecord(bodyMessage, context);
+    const region = process.env.AWS_REGION;
+    const bodyMessage = Buffer.from(event.Records[0].body, "base64").toString("binary");
+    const response = handleRequest(bodyMessage, context);
+
+    if (response.params) {
+      const outboundSqsMessage = new SendMessageCommand(response.params);
+
+      console.log(JSON.stringify(response));
+      console.log(JSON.stringify({ outboundSqsMessage }));
+
+      const sqsClient = new SQSClient({ region });
+
+      try {
+        const result = await sqsClient.send(outboundSqsMessage);
+        console.log("Success");
+        console.log(JSON.stringify(result));
+      } catch (err) {
+        console.error("Error", err);
+        throw err;
+      }
+    }
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
 
-const handleRecord = async (bodyMessage: string, context: Context) => {
-  console.log(bodyMessage);
+interface RequestResponse {
+  telegramMessage: TelegramMessage;
+  queueUrl: string;
+  params?: SendMessageCommand["input"];
+}
 
-  const sqsClient = new SQSClient({ region: process.env.AWSRegion });
+const handleRequest = (bodyMessage: string, context: Context) => {
+  const region = process.env.AWS_REGION;
+  const qname = process.env.OutboundQueueName;
+  const awsAccountID = context.invokedFunctionArn.split(":")[4];
+  const queueUrl = `https://sqs.${region}.amazonaws.com/${awsAccountID}/${qname}`;
+
   const telegramMessage: TelegramMessage = JSON.parse(bodyMessage);
-  const outgoingMessage = "hello"; //handleMessage(telegramMessage);
-
+  const outgoingMessage = handleMessage(telegramMessage).normalize('NFD').replace(/[\u0300-\u036f]/g, '');;
 
   if (outgoingMessage) {
-    console.log({ outgoingMessage });
-
-    const awsAccountID = context.invokedFunctionArn.split(":")[4];
-    const queueUrl = `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${awsAccountID}/${process.env.OutboundQueueName}`;
-
-    console.log({ queueUrl });
-
-
     const params = {
       QueueUrl: queueUrl,
       MessageBody: JSON.stringify({
@@ -44,87 +59,36 @@ const handleRecord = async (bodyMessage: string, context: Context) => {
       MessageDeduplicationId: uuidv4(),
     };
 
-    console.log({ params });
+    return {
+      telegramMessage,
+      queueUrl,
+      params,
+    };
+  }
 
-    const outboundSqsMessage = new SendMessageCommand(params);
-    
-    console.log({ outboundSqsMessage });
-    
-    sqsClient.send(outboundSqsMessage).then(async (result) => {
-      console.log("Success")
-      console.log(result)
-    }).catch((err) => {
-      console.error("Error", err)
-    })
+  return {
+    telegramMessage,
+    queueUrl,
   };
 };
 
-function handleMessage(telegramMessage: TelegramMessage) {
-  switch (telegramMessage.message.text) {
-    case "/start":
-      return welcomeMessage(telegramMessage);
-    case "/map":
-      return mapMessage(telegramMessage);
-    case "/info":
-      return infoMessage(telegramMessage);
-    default:
-      return null;
-  }
-};
-
-function welcomeMessage(telegramMessage: TelegramMessage) {
-  const message = [
+const messages: Record<string, string[]> = {
+  "/start": [
     "Merhaba, Müşahit Haritası Telegram Botuna Hoşgeldiniz!",
     new Date().toISOString(),
-    "ĞÜŞİÖÇIğüşiöçı"
-  ];
-
-  return message.join(" ");
-
-
-  // return message.reduce((acc, message, index) => {
-  //   if(index == 0) return message
-  //   return acc + " "+ message;
-  // });
-};
-
-function mapMessage(telegramMessage: TelegramMessage) {
-  const message = [
-    "[Müşahit Haritası için tıklayınız.](https://www.google.com)"
-  ];
-  return message.join(" ");
-};
-
-function infoMessage(telegramMessage: TelegramMessage) {
-  const message = [
+    "ĞÜŞİÖÇIğüşiöçı",
+  ],
+  "/map": ["[Müşahit Haritası için tıklayınız.](https://www.google.com)"],
+  "/info": [
     "Seçim surecinde gözlemci iseniz seçim bölgesine gitmeden lütfen yaninizda erzak ve mümkunse powerbank de götürün, Sayim süreçleri Sabah: 06:00 ya kadar sürebiliyor ve bazen partisel gida operasyonlari gecike biliyor.",
     "Ayni sandigin sayimina en fazla 3 kez itiraz edilebilir. Bkz Madde (Ysk Maddesi) PDF Linki:",
-    "Onceki seçimde sandik basinda 5 adet parti sandik sorumlusu var iken bu sayi 2 ye düstü bundan ötürü gözlemciler seçim seffaligi adina ok kritik önem tasiyor."
-  ];
-  return message.join(" ");
+    "Onceki seçimde sandik basinda 5 adet parti sandik sorumlusu var iken bu sayi 2 ye düstü bundan ötürü gözlemciler seçim seffaligi adina ok kritik önem tasiyor.",
+  ],
 };
 
-  // const failedMessageIds: string[] = [];
-
-  // event.Records.forEach(record => {
-  //   try {
-  //     const bodyMessage = Buffer.from(record.body, "base64").toString(
-  //       "binary"
-  //     );
-  //     handleRecord(bodyMessage, context);
-  //   } catch (error) {
-  //     console.log(error);
-  //     failedMessageIds.push(record.messageId);
-  //   }
-  // });
-
-  // return {
-  //   // https://www.serverless.com/blog/improved-sqs-batch-error-handling-with-aws-lambda
-  //   // https://docs.aws.amazon.com/prescriptive-guidance/latest/lambda-event-filtering-partial-batch-responses-for-sqs/best-practices-partial-batch-responses.html
-  //   // https://docs.aws.amazon.com/lambda/latest/dg/with-sqs.html#services-sqs-batchfailurereporting
-  //   batchItemFailures: failedMessageIds.map(id => {
-  //     return {
-  //       itemIdentifier: id
-  //     }
-  //   })
-  // }
+function handleMessage(telegramMessage: TelegramMessage) {
+  const input = telegramMessage.message.text?.toLowerCase();
+  return input == "/time"
+    ? new Date().toISOString()
+    : messages[input]?.join(" ") || input;
+}
